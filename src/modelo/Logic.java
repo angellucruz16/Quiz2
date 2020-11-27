601dfd15f91fc27ef05b37511048a76f297d6594
angellucruz16 <angellucruz16@hotmail.com>
2020-11-27 18:12:51 -0500

    COMMIT2

--- src/modelo/Logic.java
+++ src/modelo/Logic.java
@@ -1,5 +1,12 @@
 package modelo;
 
+import processing.core.PApplet;
+
 public class Logic {
+
+	public Logic (PApplet app) {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- src/vista/VistaQuiz2.java
+++ src/vista/VistaQuiz2.java
@@ -4,36 +4,58 @@ import processing.core.PApplet;
 import controlador.Controlador;
 
 public class VistaQuiz2 extends PApplet{
-	
+
 	Controlador controlador;
 
-	
-	
-public void settings() {
-		
+
+
+	public void settings() {
+
 		size(1000,500);
 	}
 
 	public void setup() {
-		
+
 		controlador = new Controlador(this);
-		
-		
-		
+
+
+
 	}
 
 	public void draw() {
 		background(255);
 		//controlador.pintar();
 		
-		
+
 	}
-public static void main(String[] args) {
-		
+	
+	
+	public static void main(String[] args) {
+
 		String[] processingArgs = {"VistaQuiz2"};
 		VistaQuiz2 mySketch = new VistaQuiz2();
 		PApplet.runSketch(processingArgs, mySketch);
 
 	} //MAIN
 
-}
+	public void keyPressed () {
+
+		if (key == CODED) {
+
+			if (keyCode == RIGHT) {
+				
+				
+
+			} if (keyCode == LEFT) {	
+				
+				
+			} 
+		} //CODED
+		
+		if (key == ' ') {
+		
+		}
+		
+	} //KEYPRESSED
+
+} //CLASE
