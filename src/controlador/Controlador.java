3dd61ae35fb5bde9804f4bebc81f0ad1321b1776
angellucruz16 <angellucruz16@hotmail.com>
2020-11-27 17:58:46 -0500

    COMMIT2
    Cree las clases

---
+++ src/controlador/Controlador.java
@@ -0,0 +1,33 @@
+package controlador;
+
+import modelo.Logic;
+import processing.core.PApplet;
+
+public class Controlador {
+
+	private PApplet app;
+	
+	
+	private Logic logic;
+
+	public Controlador (PApplet app) {
+		
+		// TODO Auto-generated constructor stub
+		this.app = app;
+		logic = new Logic(app);
+		
+	} // CONTRUCTOR
+	
+	public void disparar () {
+		
+	} //
+	
+	
+	public void render() {
+		
+	} //
+	
+
+
+
+} //CLASE
---
+++ src/vista/VistaQuiz2.java
@@ -0,0 +1,39 @@
+package vista;
+
+import processing.core.PApplet;
+import controlador.Controlador;
+
+public class VistaQuiz2 extends PApplet{
+	
+	Controlador controlador;
+
+	
+	
+public void settings() {
+		
+		size(1000,500);
+	}
+
+	public void setup() {
+		
+		controlador = new Controlador(this);
+		
+		
+		
+	}
+
+	public void draw() {
+		background(255);
+		//controlador.pintar();
+		
+		
+	}
+public static void main(String[] args) {
+		
+		String[] processingArgs = {"VistaQuiz2"};
+		VistaQuiz2 mySketch = new VistaQuiz2();
+		PApplet.runSketch(processingArgs, mySketch);
+
+	} //MAIN
+
+}
